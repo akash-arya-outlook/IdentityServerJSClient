41e5cda9b6c2f4a379638d6d77fd00f88f2a3e70
Akash-arya-igt <akash.arya@igt.in>
2020-03-05 21:43:55 +0530

    Add Test WebAPI

--- JSClientApp/wwwroot/app.js
+++ JSClientApp/wwwroot/app.js
@@ -20,9 +20,9 @@ document.getElementById("logout").addEventListener("click", logout, false);
 var config = {
     authority: "https://identityserverhostdev.webjet.com.au/",
     client_id: "CommissionGui",
-    redirect_uri: "http://localhost",
+    redirect_uri: "http://localhost:5003/callback.html",
     response_type: "code",
-    scope: "openid profile CommissionGuiApi",
+    scope: "CommissionGuiApi",
     post_logout_redirect_uri: "http://localhost:5003/index.html",
 };
 var mgr = new Oidc.UserManager(config);
@@ -43,14 +43,17 @@ function login() {
 
 function api() {
     mgr.getUser().then(function (user) {
-        var url = "http://localhost:5001/identity";
+        var url = "https://localhost:44304/values";
 
         var xhr = new XMLHttpRequest();
         xhr.open("GET", url);
         xhr.onload = function () {
             log(xhr.status, JSON.parse(xhr.responseText));
+        };
+        if (user) {
+            console.log(user.access_token);
+            xhr.setRequestHeader("Authorization", "Bearer " + user.access_token);
         }
-        xhr.setRequestHeader("Authorization", "Bearer " + user.access_token);
         xhr.send();
     });
 }
